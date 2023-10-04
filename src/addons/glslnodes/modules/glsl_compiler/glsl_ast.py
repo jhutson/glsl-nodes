bcd2437b3d53eecdead7757f48737a31ea529716
Jimmy Hutson <jimmy@hutsonandfriends.com>
2023-10-03 18:20:58 -0700

    Add basic expression and statement support to parser

--- src/addons/glslnodes/modules/glsl_compiler/glsl_ast.py
+++ src/addons/glslnodes/modules/glsl_compiler/glsl_ast.py
@@ -8,12 +8,70 @@ class Node:
         pass
 
 
+class Expression(Node):
+    pass
+
+
+@dataclass
+class ExpressionList(Node):
+    expressions: list[Expression]
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_expression_list(self)
+
+
+@dataclass
+class UnaryOpExpression(Expression):
+    expression: Expression
+    operator: str
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_unary_op_expression(self)
+
+
+@dataclass
+class BinaryOpExpression(Expression):
+    left: Expression
+    right: Expression
+    operator: str
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_binary_op_expression(self)
+
+
 @dataclass()
 class Identifier(Node):
     name: str
 
     def accept(self, visitor: 'GlslVisitor'):
         return visitor.visit_identifier(self)
+
+
+@dataclass()
+class IdentifierExpression(Expression):
+    identifier: Identifier
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_identifier_expression(self)
+
+
+@dataclass
+class AssignmentExpression(Expression):
+    left: IdentifierExpression
+    right: Expression
+    operator: str
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_assignment_expression(self)
+
+
+@dataclass
+class CallExpression(Expression):
+    name: IdentifierExpression
+    parameters: ExpressionList | None
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_call_expression(self)
 
 
 @dataclass()
@@ -41,14 +99,15 @@ class TypeDeclaration(Node):
         return visitor.visit_type_declaration(self)
 
 
-
 @dataclass
 class VariableDeclaration(Node):
     identifier: Identifier
     type: TypeDeclaration
+    initializer: Expression | None
 
     def accept(self, visitor: 'GlslVisitor'):
         return visitor.visit_variable_declaration(self)
+
 
 @dataclass
 class VariableDeclarationList(Node):
@@ -57,9 +116,71 @@ class VariableDeclarationList(Node):
     def accept(self, visitor: 'GlslVisitor'):
         return visitor.visit_variable_declaration_list(self)
 
+
+@dataclass
+class ConstantExpression(Expression):
+    type: TypeSpecifier
+
+
+@dataclass
+class IntegerExpression(ConstantExpression):
+    value: int
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_integer_expression(self)
+
+
+@dataclass
+class FloatExpression(ConstantExpression):
+    value: float
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_float_expression(self)
+
+
+@dataclass
+class BooleanExpression(ConstantExpression):
+    value: bool
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_boolean_expression(self)
+
+
+class Statement(Node):
+    pass
+
+
+@dataclass
+class StatementList(Statement):
+    statements: list[Statement]
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_statement_list(self)
+
+
+@dataclass
+class ExpressionStatement(Statement):
+    expression: Expression
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_expression_statement(self)
+
+
+@dataclass
+class FunctionDeclaration(Node):
+    name: Identifier
+    return_type: TypeDeclaration
+    parameters: list[VariableDeclaration]
+    body: Statement
+
+    def accept(self, visitor: 'GlslVisitor'):
+        return visitor.visit_function_declaration(self)
+
+
 @dataclass
 class Script(Node):
     variable_list: VariableDeclarationList
+    functions: list[FunctionDeclaration]
 
     def accept(self, visitor: 'GlslVisitor'):
         return visitor.visit_script(self)
@@ -83,10 +204,61 @@ class GlslVisitor(Node):
     def visit_variable_declaration(self, node: VariableDeclaration):
         self.visit_identifier(node.identifier)
         self.visit_type_declaration(node.type)
+        if node.initializer is not None:
+            node.initializer.accept(self)
 
     def visit_variable_declaration_list(self, node: VariableDeclarationList):
         for v in node.variables:
             self.visit_variable_declaration(v)
 
+    def visit_function_declaration(self, node: FunctionDeclaration):
+        self.visit_identifier(node.name)
+        self.visit_type_declaration(node.return_type)
+        for parameter in node.parameters:
+            parameter.accept(self)
+        node.body.accept(self)
+
+    def visit_identifier_expression(self, node: IdentifierExpression):
+        self.visit_identifier(node.identifier)
+
+    def visit_integer_expression(self, node: IntegerExpression):
+        self.visit_type_specifier(node.type)
+
+    def visit_float_expression(self, node: FloatExpression):
+        self.visit_type_specifier(node.type)
+
+    def visit_boolean_expression(self, node: BooleanExpression):
+        self.visit_type_specifier(node.type)
+
+    def visit_expression_list(self, node: ExpressionList):
+        for e in node.expressions:
+            e.accept(self)
+
+    def visit_unary_op_expression(self, node: UnaryOpExpression):
+        node.expression.accept(self)
+
+    def visit_binary_op_expression(self, node: BinaryOpExpression):
+        node.left.accept(self)
+        node.right.accept(self)
+
+    def visit_assignment_expression(self, node: AssignmentExpression):
+        node.left.accept(self)
+        node.right.accept(self)
+
+    def visit_call_expression(self, node: CallExpression):
+        self.visit_identifier_expression(node.name)
+        if node.parameters:
+            self.visit_expression_list(node.parameters)
+
+    def visit_statement_list(self, node: StatementList):
+        for s in node.statements:
+            s.accept(self)
+
+    def visit_expression_statement(self, node: ExpressionStatement):
+        node.expression.accept(self)
+
     def visit_script(self, node: Script):
         self.visit_variable_declaration_list(node.variable_list)
+        for f in node.functions:
+            f.accept(self)
+
--- src/addons/glslnodes/modules/glsl_compiler/parser.py
+++ src/addons/glslnodes/modules/glsl_compiler/parser.py
@@ -26,19 +26,64 @@ class AstBuilder(GLSLParserVisitor):
             return aggregate
         return next_result
 
-
     def visitTranslation_unit(self, ctx: GLSLParser.Translation_unitContext):
         variables = []
+        function_declarations = []
 
         for d in ctx.external_declaration():
             node = self.visitExternal_declaration(d)
             match node:
                 case ast.VariableDeclarationList():
                     variables.extend(node.variables)
+                case ast.FunctionDeclaration():
+                    function_declarations.append(node)
 
         # print(f'{variables}')
         variables_node = ast.VariableDeclarationList(variables)
-        return ast.Script(variables_node)
+        return ast.Script(variables_node, function_declarations)
+
+    def visitFunction_definition(self, ctx:GLSLParser.Function_definitionContext):
+        prototype: ast.FunctionDeclaration = ctx.function_prototype().accept(self)
+        body = ctx.compound_statement_no_new_scope().accept(self)
+
+        return ast.FunctionDeclaration(
+            prototype.name,
+            prototype.return_type,
+            prototype.parameters,
+            body
+        )
+
+    def visitFunction_prototype(self, ctx:GLSLParser.Function_prototypeContext):
+        return_type = self.visitFully_specified_type(ctx.fully_specified_type())
+        identifier_node = ast.Identifier(ctx.IDENTIFIER().symbol.text)
+
+        parameters_node = None
+        parameters = ctx.function_parameters()
+        if parameters:
+            parameters_node = parameters.accept(self)
+
+        return ast.FunctionDeclaration(identifier_node, return_type, parameters_node, ast.Statement())
+
+    def visitFunction_parameters(self, ctx:GLSLParser.Function_parametersContext):
+        parameters = [d.accept(self) for d in ctx.parameter_declaration()]
+        return ast.VariableDeclarationList(parameters)
+
+    def visitParameter_declarator(self, ctx:GLSLParser.Parameter_declaratorContext):
+        if ctx.array_specifier():
+            raise UnsupportedError("array specifier", ctx.getText())
+
+        specifier_node = self.visitType_specifier(ctx.type_specifier())
+        identifier_node = ast.Identifier(ctx.IDENTIFIER().symbol.text)
+
+        type_node = ast.TypeDeclaration(specifier_node, None)
+        return ast.VariableDeclaration(identifier_node, type_node, None)
+
+    def visitParameter_declaration(self, ctx:GLSLParser.Parameter_declarationContext):
+        first = ctx.getChild(0)
+        if isinstance(first, GLSLParser.Parameter_declaratorContext):
+            return self.visitParameter_declarator(first)
+
+        raise UnsupportedError("parameter declaration", ctx.getText())
 
     def visitInit_declarator_list(self, ctx: GLSLParser.Init_declarator_listContext):
         variable_list = []
@@ -47,8 +92,9 @@ class AstBuilder(GLSLParserVisitor):
         variable_list.append(variable_node)
 
         for declaration in ctx.typeless_declaration():
-            identifier_node = self.visitTypeless_declaration(declaration)
-            variable_list.append(ast.VariableDeclaration(identifier_node, variable_node.type))
+            identifier_node = ast.Identifier(declaration.IDENTIFIER().symbol.text)
+            initializer_node = self.visitTypeless_declaration(declaration)
+            variable_list.append(ast.VariableDeclaration(identifier_node, variable_node.type, initializer_node))
 
         return ast.VariableDeclarationList(variable_list)
 
@@ -59,16 +105,25 @@ class AstBuilder(GLSLParserVisitor):
         if typeless_declaration is None:
             raise UnsupportedError("single declaration without identifier", ctx.parentCtx.getText())
 
-        identifier_node = self.visitTypeless_declaration(typeless_declaration)
-        return ast.VariableDeclaration(identifier_node, type_node)
+        identifier_node = ast.Identifier(typeless_declaration.IDENTIFIER().symbol.text)
+        initializer_node = self.visitTypeless_declaration(typeless_declaration)
+        return ast.VariableDeclaration(identifier_node, type_node, initializer_node)
 
     def visitTypeless_declaration(self, ctx: GLSLParser.Typeless_declarationContext):
-        identifier_node = ast.Identifier(ctx.IDENTIFIER().symbol.text)
+        if ctx.array_specifier() is not None:
+            raise UnsupportedError("array specifier", ctx.getText())
 
-        # TODO: build initializer expression
-        self.visitChildren(ctx)
+        initializer = ctx.initializer()
+        if initializer is not None:
+            initializer_node = initializer.accept(self)
 
-        return identifier_node
+            # TODO: Support expressions of the form "int x = y = 3"?
+            if isinstance(initializer_node, ast.AssignmentExpression):
+                raise UnsupportedError("chained assignment in declarations", ctx.getText())
+
+            return initializer_node
+
+        return None
 
     def visitFully_specified_type(self, ctx: GLSLParser.Fully_specified_typeContext):
         specifier_node = self.visitType_specifier(ctx.type_specifier())
@@ -105,6 +160,98 @@ class AstBuilder(GLSLParserVisitor):
 
     def visitArray_specifier(self, ctx: GLSLParser.Array_specifierContext):
         raise UnsupportedError("array specifier", ctx.getText())
+
+    def visitVariable_identifier(self, ctx: GLSLParser.Variable_identifierContext):
+        identifier_node = ast.Identifier(ctx.IDENTIFIER().symbol.text)
+        return ast.IdentifierExpression(identifier_node)
+
+    def visitBinary_expression(self, ctx: GLSLParser.Binary_expressionContext):
+        op_terminal = ctx.getChild(1)
+        if op_terminal:
+            subexpressions = ctx.binary_expression()
+            left = self.visitBinary_expression(subexpressions[0])
+            right = self.visitBinary_expression(subexpressions[1])
+
+            return ast.BinaryOpExpression(left, right, op_terminal.symbol.text)
+        else:
+            result = self.visitUnary_expression(ctx.unary_expression())
+            if result is None:
+                raise UnsupportedError("unary expression", ctx.getText())
+            return result
+
+    def visitPrimary_expression(self, ctx: GLSLParser.Primary_expressionContext):
+        child = ctx.getChild(0)
+        if isinstance(child, GLSLParser.Variable_identifierContext):
+            return self.visitVariable_identifier(child)
+
+        match child.symbol.type:
+            case GLSLParser.TRUE:
+                return ast.BooleanExpression(ast.TypeSpecifier("bool"), True)
+            case GLSLParser.FALSE:
+                return ast.BooleanExpression(ast.TypeSpecifier("bool"), False)
+            case GLSLParser.INTCONSTANT:
+                # TODO: Test converting from hex or octal constant in addition to decimal
+                return ast.IntegerExpression(ast.TypeSpecifier("int"), int(child.symbol.text))
+            case GLSLParser.FLOATCONSTANT | GLSLParser.DOUBLECONSTANT:
+                # TODO: Test converting from values with exponent
+                return ast.FloatExpression(ast.TypeSpecifier("float"), float(child.symbol.text))
+            case GLSLParser.UINTCONSTANT:
+                raise UnsupportedError("uint constant", ctx.getText())
+            case GLSLParser.LEFT_PAREN:
+                return ctx.expression().accept(self)
+
+        return None
+
+    def visitAssignment_expression(self, ctx: GLSLParser.Assignment_expressionContext):
+        operator = ctx.assignment_operator()
+        if operator is None:
+            return self.visitChildren(ctx)
+
+        left = ctx.unary_expression().accept(self)
+        if left is None or not isinstance(left, ast.IdentifierExpression):
+            raise UnsupportedError("left hand side of assignment expression", ctx.unary_expression().getText())
+
+        right = ctx.assignment_expression().accept(self)
+        if right is None:
+            raise UnsupportedError("right hand side of assignment expression", ctx.assignment_expression().getText())
+
+        return ast.AssignmentExpression(left, right, operator.getChild(0).symbol.text)
+
+    def visitPostfix_expression(self, ctx: GLSLParser.Postfix_expressionContext):
+        primary = ctx.primary_expression()
+        if primary:
+            return primary.accept(self)
+
+        if ctx.LEFT_PAREN() and ctx.type_specifier() is None:
+            first = ctx.postfix_expression().accept(self)
+            if not isinstance(first, ast.IdentifierExpression):
+                raise UnsupportedError("call to non-identifier", ctx.getText())
+
+            parameters = None
+            call_parameters = ctx.function_call_parameters()
+            if call_parameters:
+                parameters = self.visitFunction_call_parameters(call_parameters)
+
+            return ast.CallExpression(first, parameters)
+
+        # TODO: support other productions for this rule.
+        raise UnsupportedError("postfix expression", ctx.getText())
+
+    def visitFunction_call_parameters(self, ctx: GLSLParser.Function_call_parametersContext):
+        if ctx.VOID():
+            raise UnsupportedError("void as parameter", ctx.getText())
+
+        parameters = [e.accept(self) for e in ctx.assignment_expression()]
+        return ast.ExpressionList(parameters)
+
+    def visitStatement_list(self, ctx:GLSLParser.Statement_listContext):
+        statement_nodes = [s.accept(self) for s in ctx.statement()]
+        return ast.StatementList(statement_nodes)
+
+    def visitExpression_statement(self, ctx:GLSLParser.Expression_statementContext):
+        expression_node = self.visitChildren(ctx)
+        if expression_node:
+            return ast.ExpressionStatement(expression_node)
 
 
 def load(file_path) -> ast.Script:
